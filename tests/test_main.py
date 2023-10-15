0897e2919268067dd138528b323d2b4fbeb47aec
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-15 14:01:01 +0300

    Test for main page was added

---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+import time
+
+import pytest
+
+from main_page import MainPage
+
+
+@pytest.fixture(scope='module')
+def main_page(oz_main_page):
+    oz_main_page: MainPage
+    oz_main_page.go_to_site()
+    yield oz_main_page
+    oz_main_page.go_to_site()
+
+def test_hide_static_books_menu(main_page):
+    main_page: MainPage
+    main_page.scroll_page_down()
+    main_page.scroll_to_books_catalog()
+    assert main_page.is_static_books_menu_pinned()
+    time.sleep(5)
+    main_page.unpin_books_catalog()
+    time.sleep(2)
+    assert main_page.is_static_books_menu_unpinned()

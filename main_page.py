75abdad4460a02db4ed9c6f048f6e516c07c0781
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-15 14:00:16 +0300

    Functions to scroll and unpin book menu was added

--- main_page.py
+++ main_page.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+from locators import MainPageLocators
+from login_page import LoginPage
 from pages import BasePage
 from search_page import Search
-from login_page import LoginPage
-from locators import MainPageLocators
 
 
 class MainPage(BasePage):
@@ -19,6 +21,20 @@ class MainPage(BasePage):
     def go_to_login(self):
         self.click_to(MainPageLocators.LOGIN_BUTTON)
         return LoginPage(self.driver, self.driver.current_url)
+
+    def unpin_books_catalog(self):
+        self.click_to((By.XPATH, "//a[@title='Открепить' and @href='/#staticmenu_header_books']"))
+
+    def is_static_books_menu_pinned(self):
+        return self.is_exists(MainPageLocators.BOOKS_CONTAINER_GRID_PIN)
+
+    def is_static_books_menu_unpinned(self):
+        return self.is_exists(MainPageLocators.BOOKS_CONTAINER_GRID_NOPIN)
+
+    def scroll_to_books_catalog(self):
+        self.scroll_to_element((By.XPATH, "//h2//a[text()='Книги']"))
+
+
 
 
 

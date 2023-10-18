fd49f508f7a7c33e299341f28e03adcf0d641b50
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-18 13:53:25 +0300

    Some tests was renamed

--- tests/test_login.py
+++ tests/test_login.py
@@ -21,13 +21,13 @@ def test_is_email_group_become_active_by_title_select(login_page):
     assert active_group_title == "Электронная почта"
     assert login_page.is_email_login_group_active()
 
-def test_try_to_log_in_with_invalid_email(login_page):
+def test_impossible_to_log_in_with_invalid_email(login_page):
     login_page.enter_email_value('kjhkhkjh')
     login_page.click_enter_button_in_email_group()
     assert login_page.is_incorrect_email_msg_exist()
     login_page.clear_filled_email_field()
 
-def test_try_to_log_in_with_invalid_password(login_page):
+def test_impossible_to_log_in_with_invalid_password(login_page):
     login_page.enter_email_value('test@test.com')
     login_page.enter_password('jgfjfj')
     login_page.click_enter_button_in_email_group()
@@ -40,7 +40,7 @@ def test_is_phone_group_become_active_by_title_select(login_page):
     assert active_group_title == "Номер телефона"
     assert login_page.is_phone_login_group_active()
 
-def test_try_to_log_in_without_phone_number(login_page):
+def test_imposibble_to_log_in_without_phone_number(login_page):
     login_page.click_send_sms_button_in_phone_group()
     assert login_page.is_phone_error_msg_correct() == 'Введите номер мобильного телефона белорусских опереторов'
     assert login_page.is_phone_error_msg_exist()

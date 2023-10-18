0098f3b8dc6499c3dc0d730db6106c11e74e3e7b
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-18 13:50:30 +0300

    const updated with browser class and headless class

--- const.py
+++ const.py
@@ -6,3 +6,16 @@ BASE_URL = 'https://www.oz.by/'
 class TIMEOUTS:
     GET_URL = 20
     FIND_ELEMENT = 20
+
+BROWSER = os.getenv('BROWSER', 'chrome')
+HEADLESS = os.getenv('HEADLESS', True)
+
+class HEADLESS_OPTION:
+    CHROME_HEADLESS = '--headless=new'
+    FIREFOX_HEADLESS = '--headless'
+
+class BROWSER_NAME:
+    CHROME = 'chrome'
+    FIREFOX = 'firefox'
+    ALL = (CHROME, FIREFOX)
+

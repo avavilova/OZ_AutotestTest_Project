dd2dc3149477a656c08a0395e541c801d4d79f6a
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-18 13:49:30 +0300

    conftest.py updated with option to choose browser

--- conftest.py
+++ conftest.py
@@ -1,17 +1,40 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Firefox, Chrome
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-from const import BASE_URL
+from const import BASE_URL, BROWSER, BROWSER_NAME, HEADLESS, HEADLESS_OPTION
 from main_page import MainPage
+
+def get_browser_class():
+    if BROWSER == BROWSER_NAME.CHROME:
+        return Chrome, ChromeService, ChromeDriverManager, ChromeOptions, HEADLESS_OPTION.CHROME_HEADLESS
+    if BROWSER == BROWSER_NAME.FIREFOX:
+        return Firefox, FirefoxService, GeckoDriverManager, FirefoxOptions, HEADLESS_OPTION.FIREFOX_HEADLESS
+    raise Exception(f'Invalid browser name={BROWSER}')
+
+
+def create_driver():
+    webdriver_class, service_class, manager_class, options_class, headless_class = get_browser_class()
+    manager = manager_class()
+    service = service_class(manager.install())
+    options = options_class()
+    if HEADLESS:
+        options.add_argument(headless_class)
+    driver = webdriver_class(options=options, service=service)
+    driver.implicitly_wait(1)
+    return driver
 
 @pytest.fixture(scope='session')
 def driver():
-    options = Options()
-    options.headless = False
-    driver = webdriver.Chrome(options=options)
-    yield driver
-    driver.quit()
+    driver_ = create_driver()
+    yield driver_
+    driver_.close()
+
 
 @pytest.fixture(scope='session')
 def oz_main_page(driver):

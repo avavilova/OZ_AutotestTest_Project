3341a39c6a9806b7f539455f5819b8480bcfbbd5
Arina Vavilova <arinavavilova29@gmail.com>
2023-10-18 13:52:01 +0300

    Comment for clickable element was added

--- pages.py
+++ pages.py
@@ -1,26 +1,17 @@
 from __future__ import annotations
 
-from locators import *
-
-import time
-
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 
 from const import TIMEOUTS
+
 
 class BasePage:
     TITLE = None
 
-    def __init__(self, driver: Chrome, url: str):
+    def __init__(self, driver, url: str):
         self.driver = driver
         self.url = url
 
@@ -32,6 +23,10 @@ class BasePage:
     def find_element(self, locator: tuple[str], timeout=TIMEOUTS.FIND_ELEMENT) -> WebElement:
         condition = EC.presence_of_element_located(locator)
         return WebDriverWait(self.driver, timeout).until(condition)
+
+    # def find_element(self, locator: tuple[str], timeout=TIMEOUTS.FIND_ELEMENT, clickable=True) -> WebElement:
+    #     condition = EC.element_to_be_clickable(locator) if clickable else EC.presence_of_element_located(locator)
+    #     return WebDriverWait(self.driver, timeout).until(condition)
 
     def find_elements(self, locator: tuple[str], timeout=TIMEOUTS.FIND_ELEMENT) -> list[WebElement]:
         condition = EC.presence_of_all_elements_located(locator)
